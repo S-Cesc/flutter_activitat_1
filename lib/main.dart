09e53ac9577b097237d1d0378232726830eeb9db
S-Cesc <cesc.sasal@gmail.com>
2024-09-29 08:52:40 +0200

    Random Names working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_name_generator/random_name_generator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    final randomNames = RandomNames(Zone.us);
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.white,
           title: const Text('Benvinguts a Flutter'),
         ),
-        body: const Center(child: Text('Hola món!')),
+        body: Center(child: Text(randomNames.fullName())),
       ),
     );
   }
